3beee5e139a0325497a77e9f1c207326e10f8175
Sebazzz <triadsebas@gmail.com>
2017-02-23 22:35:26 +0100

    Fix warning

--- os/kernel.c
+++ os/kernel.c
@@ -2,8 +2,8 @@
 
 void kmain() {
 	const char message[] = "Cee says hello!";
-	vid_print_string_line(&message);
-	vid_print_string_line(&message);
+	vid_print_string_line(message);
+	vid_print_string_line(message);
 	
 	sleep(100);
 }
