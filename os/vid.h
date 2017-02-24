df43814b46c6a7a26441545ef059570423866c3a
Sebazzz <triadsebas@gmail.com>
2017-02-24 22:35:02 +0100

    Add VS solution, not debugging support yet.

--- os/vid.h
+++ os/vid.h
@@ -5,39 +5,39 @@
 /**
  * Clears the screen using the current set attribute. Resets cursor position.
  */
-void vid_clear(void);
+extern void vid_clear(void);
 
 /**
  * Resets cursor position
  */
-void vid_reset_cursor(void);
+extern void vid_reset_cursor(void);
 
 /**
  * Set the current attribute to draw the screen with
  */
-void vid_set_attribute(unsigned int attribute);
+extern void vid_set_attribute(unsigned int attribute);
 
 /**
  * Advances the cursor as expected, to the new line if necessary
  */
-void vid_advance_cursor(void);
+extern void vid_advance_cursor(void);
 
 /**
  * Output single character to current position on screen. Advances the cursor.
  */
-void vid_put_char(char character);
+extern void vid_put_char(char character);
 
 /**
  * Output zero-terminated string to current position on screen.
  */
-void vid_print_string(const char* str);
+extern void vid_print_string(const char* str);
 
 /**
  * Output zero-terminated string to current position on screen and advances to the next line.
  */
-void vid_print_string_line(const char* str);
+extern void vid_print_string_line(const char* str);
 
 /**
 *  Advances the cursor to the next line, resets X position.
 */
-void vid_advance_line();
+extern void vid_advance_line();
