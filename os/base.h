fddde22cfbe5fc3a67c7099ed60879ef190521f0
Sebazzz <triadsebas@gmail.com>
2017-02-24 22:22:08 +0100

    Add base header for freestanding C environment

---
+++ os/base.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <float.h>
+#include <limits.h>
+#include <stdarg.h>
--- os/sdos.h
+++ os/sdos.h
@@ -2,5 +2,6 @@
 
 // sdos.h - Common functions and headers
 
+#include "base.h"
 #include "kinit.h"
 #include "vid.h"
